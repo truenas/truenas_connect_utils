66a68a2994e2f0dacd4180f238b6b3f1ef9aa4bf
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-03-24 02:45:25 +0500

    Add ability to set thread func

--- truenas_connect_utils/config.py
+++ truenas_connect_utils/config.py
@@ -1,3 +1,10 @@
+import asyncio
+from typing import Any, Callable
+
+
+THREAD_FUNC: Callable = asyncio.to_thread
+
+
 def get_account_id_and_system_id(config: dict) -> dict | None:
     jwt_details = config['registration_details'] or {}
     if all(jwt_details.get(k) for k in ('account_id', 'system_id')) is False:
@@ -7,3 +14,12 @@ def get_account_id_and_system_id(config: dict) -> dict | None:
         'account_id': jwt_details['account_id'],
         'system_id': jwt_details['system_id'],
     }
+
+
+def set_thread_func(func: Callable) -> None:
+    global THREAD_FUNC
+    THREAD_FUNC = func
+
+
+async def run_in_thread(func, *args) -> Any:
+    return await THREAD_FUNC(func, *args)
