truenas/truenas_connect_utils

commit 780b7daaf1a4b33222df2c98644068b062fc2cd3

Include response headers

truenas_connect_utils/request.py
@@ -20,6 +20,7 @@ async def call(
         'error': None,
         'response': {},
         'status_code': None,
+        'headers': {},
     }
     headers = headers or {}
     if payload is not None and (not headers or 'Content-Type' not in headers):
@@ -51,6 +52,7 @@ async def call(
     except aiohttp.ClientConnectorError as e:
         response['error'] = f'Failed to connect to TNC: {e}'
     else:
+        response['headers'] = req.headers
         if get_response:
             response['response'] = await req.json() if json_response else await req.text()
     return response

.gitignore
@@ -1,2 +1,4 @@
 /build/
 /*.egg-info/
+/.idea/
+__pycache__

commit 3102098312d38f290696c5d7f665253679c77095

Fix JSON serialization error for response headers
This commit fixes a regression introduced in 780b7da where response headers were stored as a CIMultiDictProxy object which is not JSON serializable. The fix converts headers to a plain dict.

truenas_connect_utils/request.py
@@ -52,7 +52,7 @@ async def call(
     except aiohttp.ClientConnectorError as e:
         response['error'] = f'Failed to connect to TNC: {e}'
     else:
-        response['headers'] = req.headers
+        response['headers'] = dict(req.headers)
         if get_response:
             response['response'] = await req.json() if json_response else await req.text()
     return response